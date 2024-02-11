ef078168b087f590e7ee82f71ece2ea4e06557f1
zoezmeister3695 <156547905+zoezmeister3695@users.noreply.github.com>
2024-02-10 18:58:10 -0700

    Added Lift subsystem (#1)
    * Added Lift subsystem
    
    Frame work is complete, but the motor values aren't fully known. 1 and -1 are placeholders for now.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,6 +11,7 @@ import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.subsystems.Lift;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -26,10 +27,15 @@ public class RobotContainer {
   private final CommandXboxController m_driverController =
       new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
+  Lift m_Lift;
+  
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
+
+    // Calling the lift 
+    m_Lift = new Lift();
   }
 
   /**
---
+++ src/main/java/frc/robot/subsystems/Lift.java
@@ -0,0 +1,33 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.networktables.DoubleSubscriber;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Lift extends SubsystemBase {
+  private static final TalonFX rightLiftMotor = new TalonFX(Constants.Motor.LIFTING_RIGHT);
+  private static final TalonFX leftLiftMotor = new TalonFX(Constants.Motor.LIFTING_LEFT);
+
+  /**
+   * Arms extending are positve, but doing a pull up is negative as arms are
+   * being retracted. Use as reference while coding the two movements 
+   */
+  
+  //moves the arms up to reach the chain
+  public void raising() { 
+    rightLiftMotor.set(-1);
+    leftLiftMotor.set(1);
+  }
+
+  //moves the robot up in the air
+  public void lowering() {
+    rightLiftMotor.set(1);
+    leftLiftMotor.set(-1);
+  }
+}
