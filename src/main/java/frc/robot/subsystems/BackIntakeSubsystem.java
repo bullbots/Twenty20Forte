3701c45a11be9f79bf060234833bc2f9121e6fa4
Mike L <60200998+CodeLikeAGecko@users.noreply.github.com>
2024-02-19 11:45:00 -0700

    Back intake Subsystem (#8)
    * BackIntake Subsystem created
    
    * Back Intake Subsystem Revamp
    
    used the front intake model to make the subsystem more consistent compared to the rest of the code, kept an isOn() feature not in the front intake Subsystem
    
    * Fixed Constant Number and renamed some variabled
    
    changed m_backIntakeMotor to BeltMotor and INTAKE_BACK to BELT to make it match the docs page
    
    * Added Second Belt motor and Fixed constant number
    
    The motor has is inverse to what the first belt motor is. For Constants BELT1 = 10 , BELT2 =11. (belt 2 is the new one)
    
    * Simplified isOn() boolean code
    
    * Name fixes for consistency
    
    * Update BackIntakeSubsystem.java
    
    ---------
    
    Co-authored-by: Carson Going <117052774+GarlicBreadPog@users.noreply.github.com>

---
+++ src/main/java/frc/robot/subsystems/BackIntakeSubsystem.java
@@ -0,0 +1,46 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.led.ColorFlowAnimation.Direction;
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class BackIntakeSubsystem extends SubsystemBase {
+    TalonSRX m_BeltMotor1;
+    TalonSRX m_BeltMotor2;
+    int m_MotorDirection = 0;
+
+    public BackIntakeSubsystem() {
+        m_BeltMotor1 = new TalonSRX(Constants.Motors.BELT1);
+        m_BeltMotor2 = new TalonSRX(Constants.Motors.BELT2);
+    }
+
+
+    public void enableIntake() {
+        m_BeltMotor1.set(TalonSRXControlMode.PercentOutput, (m_MotorDirection*2)-1);
+        m_BeltMotor2.set(TalonSRXControlMode.PercentOutput, -((m_MotorDirection*2)-1));
+    }
+
+    public void disableIntake() {
+        m_BeltMotor1.set(TalonSRXControlMode.PercentOutput, 0);
+        m_BeltMotor2.set(TalonSRXControlMode.PercentOutput, 0);
+    }
+    /**
+     * Sets the direction of the front intake motor
+     * 
+     * @param direction 0 is forward, 1 is backward
+     */
+    public void setIntakeDirection(int direction) {
+        m_MotorDirection = direction;
+    }
+
+    public int getIntakeDirection() {
+        return m_MotorDirection;
+    }
+    public boolean isEnabled(){
+        return m_BeltMotor1.getMotorOutputPercent()!=0;
+    }
+
+}
