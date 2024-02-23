3b2844b2c4bb4c9a5a50f384a1b4522960f9e7ad
zoezmeister3695 <zoe.watts.id@gmail.com>
2024-02-22 19:23:00 -0700

    Added the Slider command
    I think this command will work, idk though cause I can't reference the slide subsystem at all

--- src/main/java/frc/robot/commands/MoveSlide.java
+++ src/main/java/frc/robot/commands/MoveSlide.java
@@ -3,11 +3,11 @@
 // the WPILib BSD license file in the root directory of this project.
 package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.Slide;
 
-public class LoadInShooter extends Command {
+public class MoveSlide extends Command {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final Shooter m_shooterSubsystem;
+  private final Slide m_slideSubsystem;
   
   private int m_direction;
 
@@ -17,21 +17,22 @@ public class LoadInShooter extends Command {
    * @param Shooter The subsystem used by this command.
    * @return 
    */
-  public Loading(Shooter shoot, int direction) {
+  public Sliding(Slide slide, int direction) {
     m_direction = direction;
-    m_shooterSubsystem = shoot;
+    m_slideSubsystem = slide;
     
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(shoot);
-    //Robot arms retracting in
-    m_shooterSubsystem.enable();
+    addRequirements(slide);
+    //enabled
+    m_slideSubsystem.setDirection(Down);
+    m_slideSubsystem.enable();
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_shooterSubsystem.setDirection(m_direction);
-    m_shooterSubsystem.enable();
+    m_slideSubsystem.setDirection(m_direction);
+    m_slideSubsystem.enable();
   }
     
   // Called every time the scheduler runs while the command is scheduled.
@@ -43,7 +44,7 @@ public class LoadInShooter extends Command {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    m_shooterSubsystem.disable();
+    m_slideSubsystem.disable();
   }
 
   // Returns true when the command should end.
