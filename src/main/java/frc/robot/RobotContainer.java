71c7691c8045b5a047134c4af6fd21873c1a0d86
Carson Going <117052774+GarlicBreadPog@users.noreply.github.com>
2024-02-22 19:13:12 -0700

    Fixed Lift Direction

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -81,8 +81,8 @@ public class RobotContainer {
 
     //Buttons for co-driver moving the slider up and down
     //Slider up
-    m_guitarHero.povDown().whileTrue(new Slide(Up, 180));
-    m_guitarHero.povUp().whileTrue(new Slide(Down, 0));
+    m_guitarHero.povDown().whileTrue(new Slide(Down, 180));
+    m_guitarHero.povUp().whileTrue(new Slide(Up, 0));
     
   }
   
