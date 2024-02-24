a3dbdc43374a4b68186223c7bf843de07a56fdb2
Carson Going <117052774+GarlicBreadPog@users.noreply.github.com>
2024-02-24 13:45:57 -0700

    backIntake cleanup

--- src/main/java/frc/robot/subsystems/BackIntake.java
+++ src/main/java/frc/robot/subsystems/BackIntake.java
@@ -9,7 +9,6 @@ import frc.robot.Constants;
 
 public class BackIntake extends SubsystemBase {
     TalonSRX m_BeltMotor;
-    TalonSRX m_BeltMotor2;
     int m_Direction = 1;
     private static BackIntake instance = null;
     public BackIntake getInstance(){
