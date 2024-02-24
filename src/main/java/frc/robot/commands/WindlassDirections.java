5988fe2ab167ecea49670454422fc467309e73e8
p3nnylane <natibryant256@gmail.com>
2024-02-24 13:01:16 -0700

    Updated command
    Switched command to it's own branch.

---
+++ src/main/java/frc/robot/commands/WindlassDirections.java
@@ -0,0 +1,14 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.Windlass;
+
+
+public class WindlassDirections {
+
+  m_Windlass = new Windlass();
+
+    // Creating left lift arm
+    m_Windlass = new Windlass(Constants.Motors.WINDLASS);
+}
