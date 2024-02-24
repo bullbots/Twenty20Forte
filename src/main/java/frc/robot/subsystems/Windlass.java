0be6bb4ba4139ac0adaa379d8d4c2f3fda1b1586
p3nnylane <natibryant256@gmail.com>
2024-02-24 11:08:35 -0700

    Final windlass subsystem
    Finished the windlass subsystem

--- src/main/java/frc/robot/subsystems/Windlass.java
+++ src/main/java/frc/robot/subsystems/Windlass.java
@@ -1,63 +1,56 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
+// forward is negative and back is positive 
 package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 import frc.robot.motors.WPI_CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
 public class Windlass extends SubsystemBase {
 
-  private WPI_CANSparkMax m_windlassMotor;
-  private int m_motorID;
-  /** Creates a new ExampleSubsystem. */
-  public Windlass() {}
 
-  public Windlass(int motorID) {
-    m_windlassMotor = new WPI_CANSparkMax(motorID, );
-    m_motorID = motorID;
+  private static final WPI_CANSparkMax m_WindlassMotor = new WPI_CANSparkMax(Constants.Motors.WINDLASS, MotorType.kBrushless); 
+  int m_Direction = 1;
+
+  /** The constructor */
+  public Windlass() {
+    
   }
 
 
+
+  
 
   public void enable() {
-    m_
+    // set it to a quarter speed
+    m_WindlassMotor.set(m_Direction * 0.25);
   }
 
-
-  /**
-   * Example command factory method.
-   *
-   * @return a command
-   */
-  public Command exampleMethodCommand() {
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return runOnce(
-        () -> {
-          /* one-time action goes here */
-        });
+  public void disable() {
+    m_WindlassMotor.set(0);
   }
+    /**
+     * Sets the direction of the front intake motor
+     * 
+     * @param direction 0 is forward, 1 is backward
+     */
+    public void setDirection(int direction) {
+        m_Direction = direction;
+    }
 
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
-  public boolean exampleCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
-  }
+    public int getDirection() {
+        return m_Direction;
+    }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
-
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
-  }
+    public boolean isEnabled(){
+        return false; 
+    }
 }
+
+
