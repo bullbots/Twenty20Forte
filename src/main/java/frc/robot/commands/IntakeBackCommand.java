7c711a97e74adbac225e74fff2deb8099418e88a
Anson Kiehl <100244371+Z-Ninja-Monkey@users.noreply.github.com>
2024-02-26 19:55:03 -0700

    Updated Intake front and back commands, and updated robot container

--- src/main/java/frc/robot/commands/IntakeBackCommand.java
+++ src/main/java/frc/robot/commands/IntakeBackCommand.java
@@ -1,5 +1,7 @@
 package frc.robot.commands;
 
+import java.util.function.BooleanSupplier;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.BackIntake;
 import frc.robot.subsystems.FrontMiddleIntake;
@@ -12,35 +14,49 @@ public class IntakeBackCommand extends Command {
     int m_Direction;
     FrontMiddleIntake m_FrontMiddleIntake;
     Stager m_Stager;
+    BooleanSupplier m_Sensor;
 
-    public IntakeBackCommand(BackIntake backIntake, FrontMiddleIntake frontMiddleIntake, Stager stager, int direction) {
+    public IntakeBackCommand(BackIntake backIntake, FrontMiddleIntake frontMiddleIntake, Stager stager, int direction, BooleanSupplier sensor) {
         m_BackIntake = backIntake;
         m_FrontMiddleIntake = frontMiddleIntake;
         m_Direction = direction;
         m_Stager = stager;
+        m_Sensor = sensor;
 
     }
 
     @Override
     public void initialize() {
 
+        if (m_Sensor.getAsBoolean()){
+            System.out.println("note/ring/donut/donote/orange thingy sensed");
+            return;
+        }
         m_BackIntake.setDirection(m_Direction);
         m_FrontMiddleIntake.setDirection(-m_Direction);
 
         m_BackIntake.start();
         m_FrontMiddleIntake.start();
-
-        if (m_Direction == 0) {
-            m_Stager.stop();
-        } else {
-            m_Stager.start(Stager.Mode.MAX_SPEED);
-        }
+        m_Stager.start(Stager.Mode.MAX_SPEED);
         System.out.println("back intake initialized");
+
+    }
+
+    @Override
+    public void execute(){
+
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        m_BackIntake.stop();
+        m_FrontMiddleIntake.stop();
+        m_Stager.stop();
     }
 
     @Override
     public boolean isFinished() {
-        return true;
+        return m_Sensor.getAsBoolean();
     }
 }
 
--- src/main/java/frc/robot/commands/SetIntakeFront.java
+++ src/main/java/frc/robot/commands/SetIntakeFront.java
@@ -3,6 +3,10 @@ package frc.robot.commands;
 
 import frc.robot.subsystems.FrontMiddleIntake;
 import frc.robot.subsystems.Stager;
+
+import java.util.function.BooleanSupplier;
+
+import edu.wpi.first.networktables.BooleanSubscriber;
 import edu.wpi.first.wpilibj2.command.Command;
 
 
@@ -11,31 +15,39 @@ public class SetIntakeFront extends Command {
     FrontMiddleIntake m_Intake;
     int m_Direction;
     Stager m_Stager;
+    BooleanSupplier m_Sensor;
 
-    public SetIntakeFront(FrontMiddleIntake intake, Stager stager, int direction) {
+    public SetIntakeFront(FrontMiddleIntake intake, Stager stager, int direction, BooleanSupplier sensor) {
         m_Direction = direction;
         m_Intake = intake;
         m_Stager = stager;
+        m_Sensor = sensor;
         addRequirements(m_Intake);
     }
 
     @Override
     public void initialize() {
-        m_Intake.setDirection(m_Direction);
-        m_Intake.start();
-        System.out.println("front intake at speed:" + m_Direction);
-        if (m_Direction == 0) {
-            m_Stager.stop();
-        } else {
-            m_Stager.start(Stager.Mode.MAX_SPEED);
+
+        if (m_Sensor.getAsBoolean()){
+            System.out.println("note/ring/donut/donote/orange thingy sensed");
+            return;
         }
 
-        System.out.println("back intake initialized");
+        m_Intake.setDirection(m_Direction);
+        m_Intake.start();
+        m_Stager.start(Stager.Mode.MAX_SPEED);
+        
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        m_Intake.stop();
+        m_Stager.stop();
     }
 
     @Override
     public boolean isFinished() {
-        return true;
+        return m_Sensor.getAsBoolean();
     }
 
 }
