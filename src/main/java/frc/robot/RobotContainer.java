5fb7521f4173e1475707dfb86b6f54516f041745
Warren Parsons <parsonswarren30@gmail.com>
2024-03-16 09:03:16 -0600

    Minon changes to robot oriented button controller.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,10 +6,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.*;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -143,13 +140,10 @@ public class RobotContainer {
                 m_driverController.rightTrigger(0.5).onFalse(new KillAll());
                 m_driverController.leftTrigger(0.5).whileTrue(new ShootInAmp());
 
-                m_driverController.back().onTrue(new InstantCommand() {
-                        @Override
-                        public void initialize() {
-                                fieldOrientation.toggleOrientation();
-                                System.out.println("Field relative set to" + fieldOrientation.isFieldRelative());
-                        }
-                });
+                m_driverController.back().onTrue(Commands.runOnce(() -> {
+                        fieldOrientation.toggleOrientation();
+                        System.out.println("Field relative set to" + fieldOrientation.isFieldRelative());
+                }));
                 //m_driverController.back().( )
                 // m_driverController.a().onTrue(new SlideSliderToPosition(slider, 1,
                 // slider::isAtPosition));
