a3e3bad9f2b2d78c42006595552d6b6d95a360b6
zoezmeister3695 <zoe.watts.id@gmail.com>
2024-03-22 12:01:01 -0600

    moar stuff yay

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -247,5 +247,9 @@ public class RobotContainer {
 
     public void periodic() {
         SmartDashboard.putNumber("Intake Sensor", m_intakeSensor.get() ? 1 : 0);
+
+        if (m_intakeSensor.get()) {
+            new SolidColor(leds, Constants.LED_COUNT, 255, 165, 0).schedule();
+        }
     }
 }
