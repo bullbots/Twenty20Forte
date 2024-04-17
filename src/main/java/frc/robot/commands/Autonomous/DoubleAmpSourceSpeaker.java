012dd57366cbd626f41dd3019a77d0bdaf53957a
Bullbots Lap <100244371+Z-Ninja-Monkey@users.noreply.github.com>
2024-04-17 11:42:33 -0600

    Untested Source/Amp Side Double Speaker
    it might work if my brain is working idk

---
+++ src/main/java/frc/robot/commands/Autonomous/DoubleAmpSourceSpeaker.java
@@ -0,0 +1,40 @@
+package frc.robot.commands.Autonomous;
+
+import edu.wpi.first.wpilibj2.command.*;
+import frc.robot.RobotContainer;
+import frc.robot.commands.KillAll;
+import frc.robot.commands.SetIntakeFront;
+import frc.robot.commands.drivetrain.TurningRobotFuzzyLogic;
+import frc.robot.commands.shooting.ShootInSpeaker;
+
+public class DoubleAmpSourceSpeaker extends SequentialCommandGroup {
+
+    public enum SpeakerSide {
+        BLUE,
+        RED,
+        AMP,
+        SOURCE
+    }
+
+    public DoubleAmpSourceSpeaker(SpeakerSide ampSide, SpeakerSide sourceSide) {
+
+        var ampTurnSign = (ampSide == SpeakerSide.BLUE) ? 1 : -1;
+        var sourceTurnSign = (sourceSide == SpeakerSide.AMP) ? 1 : -1;
+        var driveTime = 0.5;
+
+        addCommands(
+                new WaitCommand(3.0),
+                new ParallelDeadlineGroup(new WaitCommand(3.0), new ShootInSpeaker()),
+                new KillAll(),
+                new DriveForward(driveTime),
+                new TurningRobotFuzzyLogic(ampTurnSign * sourceTurnSign * 50),
+                new ParallelRaceGroup(
+                        new SetIntakeFront(1, RobotContainer.m_intakeSensor::get),
+                        new DriveForward(1.1)),
+                new DriveBackward(1),
+                new TurningRobotFuzzyLogic(ampTurnSign * sourceTurnSign * -50),
+                new WaitCommand(0.5),
+                new ShootInSpeaker()
+        );
+    }
+}
